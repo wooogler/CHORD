fc1fdfa93130aa5753a1974ecce34701bcf34d4c
Sangwook Lee <leesang627@gmail.com>
2024-09-25 17:58:55 -0400

    use react-contenteditable

--- components/base-editor.tsx
+++ components/base-editor.tsx
@@ -1,20 +1,23 @@
 "use client";
 
 import React, { useState } from "react";
+import ContentEditable from "react-contenteditable";
 
 export default function BaseEditor({ articleHtml }: { articleHtml: string }) {
   const [content, setContent] = useState(articleHtml);
 
   const handleChange = (evt: React.FormEvent<HTMLDivElement>) => {
     setContent(evt.currentTarget.innerHTML);
+    console.log(content);
   };
 
   return (
     <div className="grid grid-cols-[1fr_600px]">
-      <div
-        dangerouslySetInnerHTML={{ __html: articleHtml }}
-        contentEditable
+      <ContentEditable
         className="p-4"
+        html={content}
+        disabled={false} // Set to true to make it read-only
+        onChange={handleChange}
       />
       <div>(Chat for Chord)</div>
     </div>
