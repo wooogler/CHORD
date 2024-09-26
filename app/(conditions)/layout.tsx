4c086231d4ac872f879c60cd9529ee5221aa979d
mbrenn <mbrenn@vt.edu>
2024-09-26 11:22:51 -0400

    added article additions to sidebar

--- app/(conditions)/layout.tsx
+++ app/(conditions)/layout.tsx
@@ -2,46 +2,15 @@
 
 import ConditionNav from "@/components/condition-nav";
 import SideNav from "@/components/side-nav";
-import { usePathname, useRouter, useSearchParams } from "next/navigation";
-
 
 interface EditorLayoutProps {
   children: React.ReactNode;
 }
 
 export default function EditorLayout({ children }: EditorLayoutProps) {
-  const searchParams = useSearchParams();
-  const pathname = usePathname();
-  const { replace } = useRouter();
-
-  const handleSearch = (title: string) => {
-    const params = new URLSearchParams(searchParams);
-    if (title) {
-      params.set("title", title);
-    } else {
-      params.delete("title");
-    }
-    replace(`${pathname}?${params.toString()}`);
-  };
-
   return (
     <main className="grid grid-cols-[250px_1fr] min-h-screen p-8 gap-4">
       <aside className="border rounded-lg p-4 overflow-y-auto">
-        <h2 className="text-lg font-bold mb-4">Wikipedia Articles</h2>
-        <form>
-          <input
-            type="search"
-            placeholder="Search Wikipedia articles"
-            className="w-full p-2 mb-4 border rounded"
-            onKeyDown={(e) => {
-              if (e.code === "Enter") {
-                //@ts-expect-error there is a value present
-                handleSearch(e.target.value)
-                e.preventDefault()
-              }
-            }}
-          />
-        </form>
         <SideNav />
       </aside>
       <div className="flex flex-col">
