198d936466ec6aeb3ac1a59fb867de10f624da8a
Sangwook Lee <leesang627@gmail.com>
2024-12-04 15:31:12 -0500

    Add reference to edit Wikipedia article.

--- components/norms-list.tsx
+++ components/norms-list.tsx
@@ -1,10 +1,58 @@
 export default function NormsList() {
   return (
     <>
-      <div className="text-xl font-bold">Wikipedia Norms</div>
+      <div className="text-xl font-bold">
+        Reference to edit Wikipedia articles
+      </div>
       <ul className="list-disc ml-4">
-        <li>Norm 1</li>
-        <li>Norm 2</li>
+        <li>
+          <a
+            href="https://en.wikipedia.org/wiki/Wikipedia:Five_pillars"
+            target="_blank"
+          >
+            Wikipedia:Five pillars
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://en.wikipedia.org/wiki/Wikipedia:Neutral_point_of_view"
+            target="_blank"
+          >
+            Wikipedia:Neutral point of view
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://en.wikipedia.org/wiki/Wikipedia:Wikipedia_is_not_a_dictionary"
+            target="_blank"
+          >
+            Wikipedia:Wikipedia is not a dictionary
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://en.wikipedia.org/wiki/Wikipedia:Be_bold"
+            target="_blank"
+          >
+            Wikipedia:Be bold
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://en.wikipedia.org/wiki/Wikipedia:Expectations_and_norms_of_the_Wikipedia_community"
+            target="_blank"
+          >
+            Wikipedia:Expectations and norms of the Wikipedia community
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://en.wikipedia.org/wiki/Wikipedia:Good_article_criteria"
+            target="_blank"
+          >
+            Wikipedia:Good article criteria
+          </a>
+        </li>
       </ul>
     </>
   );
